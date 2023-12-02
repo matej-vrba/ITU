6595bfc9cd85c143c3632818cd23e6dbef480924
Mates Vrba <xvrbam03@stud.fit.vutbr.cz>
2023-12-02 22:45:04 +0100

    fix: added plus icon I forgot to include

---
+++ frontend/src/icons/Plus.js
@@ -0,0 +1,24 @@
+
+
+function PlusIcon(){
+  return(
+<svg
+    className={"icon-svg"}
+   width="0.9em"
+   viewBox="0 0 46.04187 46.04187"
+   version="1.1" >
+  <g>
+    <g>
+      <path
+         d="M 3.8828242,22.401954 H 42.92469"
+         id="path1" />
+      <path
+         d="M 23.403757,2.8810213 V 41.922887"
+         id="path2" />
+    </g>
+  </g>
+</svg>
+  )
+
+}
+export default PlusIcon;
